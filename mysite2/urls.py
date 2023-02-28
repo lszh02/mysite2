bbcc18f37a6c93bc661d7890ffea9634f3157783
lszh01 <464575284@qq.com>
2023-03-01 00:28:50 +0800

    部门列表前端展示

--- mysite2/urls.py
+++ mysite2/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from web2 import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('depart/list', views.depart_list),
 ]
--- web2/views.py
+++ web2/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def depart_list(request):
+    """部门列表"""
+
+    return render(request, 'depart_list.html')
